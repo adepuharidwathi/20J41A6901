12304257eb74de224ff4e6c306b2d8cce0384f7d
adepuharidwathi <114178764+adepuharidwathi@users.noreply.github.com>
2023-09-23 14:36:39 +0530

    Add files via upload

---
+++ number.management/number.management/src/main/java/com/number/management/number/management/Application.java
@@ -0,0 +1,15 @@
+package com.number.management.number.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.number.management.number.management"})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ number.management/number.management/src/main/java/com/number/management/number/management/numbermanagement/controller/NumberManagement.java
@@ -0,0 +1,52 @@
+package com.number.management.number.management.numbermanagement.controller;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class NumberManagement {
+	
+	@RequestMapping("/hello")
+	public String helloworld() {
+	return "hello world";
+	}
+	
+	@RequestMapping("/primenumbers")
+	public String primenumbers() {
+		ArrayList<Integer> listOfPrimes= new ArrayList<Integer>();
+		int N = 100;
+        for(int i=1; i<=N; i++){
+            if(isPrime(i)) {
+            	listOfPrimes.add(i);
+                }    
+	}
+       
+        return listOfPrimes.toString();
+	}
+            
+           private boolean isPrime(int n){
+                if(n==1||n==0)return false;
+                for(int i=2; i<n; i++){
+                      if(n%i==0)return false;
+                }
+                return true;
+          }
+           
+           @RequestMapping("/oddnumbers")
+       	public String oddnumbers() {
+       		ArrayList<Integer> listOfOdd= new ArrayList<Integer>();
+       		int N = 100;
+               for(int i=1; i<=N; i++){
+                   if(i%2!=0) {
+                   	listOfOdd.add(i);
+                       }       
+       	} 
+               return listOfOdd.toString();
+           	}
+
+}
