0c6c281677f4ad98032f84e7e728af5375cc6d0e
adepuharidwathi <114178764+adepuharidwathi@users.noreply.github.com>
2023-09-23 13:05:28 +0530

    Add files via upload

---
+++ number.management/src/main/java/com/number/management/number/management/Application.java
@@ -0,0 +1,13 @@
+package com.number.management.number.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
